2bddaf8958e369f0a990248e77916acc7fab71ba
sun-5 <987887117@qq.com>
2020-09-04 21:16:37 +0800

    2020/9/4/21:16

--- php/3.php
+++ php/3.php
@@ -5,9 +5,24 @@ echo
 <h3>php常量</h3>
 asdasd
 asdasd
-"
+";
 
 
-
+echo print('dasgasd');
+print '|45656';
+print_r('|4556867as');
+echo "<hr>";
+// date();//从1970年格林威治时间开始计算的秒数
+$a = time();//当前时间对应的时间戳
+echo $a;
+echo "<hr>";
+echo date('Y-m-d');
+echo "<hr>";
+echo microtime();//当前Unix时间戳以及微妙数
+echo "<hr>";
+echo date("Y 年 m月d日 H:i:s",12345678) ;
+echo "<hr>";
+echo strtotime('tomorrow 10 hours');
+echo "<hr>";
 
 ?>
